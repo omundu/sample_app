feb5f4e65e620c713610b5ea561d0bc13d45586f
Mwaki Magotswi <magotswi@gmail.com>
2011-06-27 18:25:11 -0400

    fully tested new page

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = { :name => "Example User",
-              :email => "user@example.com",
-              :password => "foobar",
-              :password_confirmation => "foobar"
+    @attr = { :name                   => "Example User",
+              :email                  => "user@example.com",
+              :password               => "foobar",
+              :password_confirmation  => "foobar"
               }
   end
   
