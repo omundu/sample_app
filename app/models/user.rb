e0580fa2f8201899b167440d391e1753a65cbdd9
Mwaki Magotswi <magotswi@gmail.com>
2011-07-26 09:06:39 -0400

    dealt with warnings

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,15 +35,15 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
   
   #Return true if the user's password mathces the submitted password.
-  def has_password? (submitted_password)
+  def has_password?(submitted_password)
     #compare encrypted_password with the encrypted version of submitted password
     encrypted_password == encrypt(submitted_password)
   end
   
-  def self.authenticate (email, submitted_password)
+  def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil if user.nil?
-    return user if user.has_password? (submitted_password)
+    return user if user.has_password?(submitted_password)
   end
   
   def self.authenticate_with_salt(id, cookie_salt)
